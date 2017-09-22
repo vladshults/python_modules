6e905f172b580ad3cd80aa2310dd1f678f700537
vladshults <vladshulkevich@gmail.com>
2017-09-22 14:32:12 +0300

    asdfas

--- ctxt_switching_rate.bash
+++ ctxt_switching_rate.bash
@@ -19,7 +19,7 @@ SWITCHING_RATE=$(echo "scale=0; $CTXTDELTA/$TIMEDELTA" | bc -l)
 SWITCHING_SPEED=$(echo "scale=8; $TIMEDELTA/$CTXTDELTA" | bc -l)
 
 echo "During $TIMEDELTA seconds we found context switching amount per 1 second:"
-echo $SWITCHING_RATE
+echo "   $SWITCHING_RATE"
 echo "One switching takes amount of time in seconds:"
-echo $SWITCHING_SPEED
+echo "   $SWITCHING_SPEED"
 echo ""
