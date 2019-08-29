4241d67149887f8edc0636f7cb4fdbcb22e8e98b
vladshults <vladshulkevich@gmail.com>
2019-08-29 14:33:50 +0300

    Create repeatings.py

---
+++ job_interview_algs/repeatings.py
@@ -0,0 +1,24 @@
+TEXT = """abba com mother bill mother com 
+abba dog abba mother com"""
+
+
+def secuenced_words(txt):
+    """
+        Function identifies and displays the three words
+        most often repeated as a group, regardless of the
+        words order in the group
+    """
+    word_list = txt.split()
+    collector = dict()
+    for idx in range(1, len(word_list)-1):
+        item = frozenset([word_list[idx-1], word_list[idx], word_list[idx+1]])
+        if item not in collector:
+            collector[item] = 1
+        else:
+            collector[item] += 1
+
+    return list(sorted(collector)[0])
+
+
+if __name__ == "__main__":
+    print(secuenced_words(TEXT))
